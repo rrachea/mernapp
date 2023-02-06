9a909cf9c8912804757c2c48a4ed078141b6d4da
rrachea <rrachelsng@gmail.com>
2023-02-06 21:02:49 +0800

    add gh pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={callApi}>Call API</button>
+        </header>
+      </div>
   );
+}
+
+function callApi() {
+    fetch('http://localhost:5000/details', { method: 'GET' })
+        .then(data => data.json())
+        .then(json => alert(JSON.stringify(json)))
 }
 
 export default App;
